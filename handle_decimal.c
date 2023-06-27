0f3a4b35a04abdfdb1467ab70af0782dffeb6739
Ali Mohamed <mohamed37222721@gmail.com>
2023-06-27 10:27:27 +0200

    fixing handle_int bugs

--- handle_decimal.c
+++ handle_decimal.c
@@ -6,17 +6,23 @@
  */
 int handle_decimal(va_list list)
 {
-	int length = 1, s = 1, sum = 0, n, num, out, l;
+	int length = 1, s = 1, sum = 0, n, num, out, l, t, h = 0;
 
 	num = va_arg(list, int);
 	if (num < 0)
 	{
 		cout('-');
-		num = num * -1;
-		length++;
+		num = num * -1, length++;
 	}
-	l = num % 10;
-	num = num / 10;
+	l = num % 10, num = num / 10, n = num;
+	if (n / 10 != 0)
+	{
+		do {
+			t = n % 10;
+			if (t == 0)
+				n = n / 10, h++;
+		} while (t == 0 && n / 10 != 0);
+	}
 	n = num;
 	while (n / 10 != 0)
 		n = n / 10, s = s * 10;
@@ -30,6 +36,11 @@ int handle_decimal(va_list list)
 	{
 		cout((sum % 10) + '0');
 		sum = sum / 10, length++;
+		if (sum == 0)
+		{
+			for (; h; length++, h--)
+				cout('0');
+		}
 	}
 	cout(l + '0');
 	return (length);
